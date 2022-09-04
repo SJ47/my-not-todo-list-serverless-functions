1f372413bcf37962045b43a7eb602fd913848325
Scott Johnston <scott@umachan.co.uk>
2022-09-04 13:16:31 +0100

    Fixed missing db name

--- netlify/functions/read-all.js
+++ netlify/functions/read-all.js
@@ -5,7 +5,7 @@ const uri = process.env.MONGO_DB_URI;
 
 exports.handler = async function (event, context) {
     const client = await mongodb.connect(`mongodb+srv://${username}:${password}@${uri}?retryWrites=true&w=majority`, { useUnifiedTopology: true })
-    const db = client.db()
+    const db = client.db("tasks_hub")
 
     try {
         const tasks = await db.collection("tasks")
