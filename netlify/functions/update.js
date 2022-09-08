091fd5901e2813ff42816d00950d103092053df5
Scott Johnston <scott@umachan.co.uk>
2022-09-08 20:39:40 +0100

    Refactor config setup

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,13 +1,4 @@
-const AWS = require('aws-sdk');
-const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID;
-const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY;
-const REGION = process.env.REGION;
-
-// Set a region to interact with (make sure it's the same as the region of your table)
-AWS.config.update({ region: REGION, accessKeyId: ACCESS_KEY_ID, secretAccessKey: SECRET_ACCESS_KEY });
-
-// Create the Document Client interface for DynamoDB
-var ddbDocumentClient = new AWS.DynamoDB.DocumentClient();
+const ddbDocumentClient = require("./config")
 
 exports.handler = async function (event, context, callback) {
     const data = JSON.parse(event.body)
